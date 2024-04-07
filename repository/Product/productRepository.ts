306503bc9804125b2c42af8ec082bf69f371f290
Mazaev Andrew <mazaev2000@gmail.com>
2024-04-07 11:20:34 +0300

    feat: all properties extends from BaseRepo, Omit function for hide unnecessary properties in child classes

--- repository/Product/productRepository.ts
+++ repository/Product/productRepository.ts
@@ -1,69 +1,28 @@
-import type { _AsyncData } from 'nuxt/dist/app/composables/asyncData'
-import type { FetchError } from 'ofetch'
-import type { UseFetchOptions } from 'nuxt/app'
+import type { _AsyncData, KeysOf } from 'nuxt/dist/app/composables/asyncData'
 import BaseRepository from '../baseRepository'
-import type {
-  IProduct,
-  IProductsData,
-  IProductRepository,
-  IProductCreateDataForm,
-} from './types.product'
+import type { IProduct } from './types.product'
+import type { UseFetchOptions } from 'nuxt/dist/app/composables'
+import type { FetchError } from 'ofetch'
 
 const productEndpoint = '/product'
 
-class ProductRepository
-  extends BaseRepository<IProduct>
-  implements IProductRepository
-{
+/**
+ * Generates a new class based on the input class with specified keys omitted.
+ *
+ * @param {new (_endpoint: string) => T} Class - The input class to derive from.
+ * @param {K[]} keys - The keys to omit from the new class.
+ * @return {new (_endpoint: string) => Omit<T, (typeof keys)[number]>} The new class with specified keys omitted.
+ */
+const Omit = <T, K extends keyof T>(
+  Class: new (_endpoint: string) => T,
+  keys: K[]
+): new (_endpoint: string) => Omit<T, (typeof keys)[number]> => Class
+
+class ProductRepository extends Omit(BaseRepository<IProduct, FormData>, []) {
   constructor() {
     super(productEndpoint)
-  }
-
-  public async getAllProducts(): Promise<
-    _AsyncData<IProductsData | null, FetchError<any> | null>
-  > {
-    return await this._handleRequest<IProductsData>(this._endpoint, 'GET')
-  }
-
-  public async getOneProduct(
-    id: number
-  ): Promise<_AsyncData<IProduct | null, FetchError<any> | null>> {
-    return await this._handleRequest<IProduct>(this._endpoint + '/' + id, 'GET')
-  }
-
-  public async createProduct(
-    product: FormData
-  ): Promise<_AsyncData<IProduct | null, FetchError<any> | null>> {
-    return await this._handleRequest<IProduct, FormData>(
-      this._endpoint,
-      'POST',
-      {
-        body: product,
-      }
-    )
-  }
-
-  public async updateProduct(
-    id: string,
-    product: IProduct
-  ): Promise<_AsyncData<IProduct | null, FetchError<any> | null>> {
-    return await this._handleRequest<IProduct>(
-      this._endpoint + '/' + id,
-      'PUT',
-      {
-        body: product,
-      }
-    )
-  }
-
-  public async deleteProduct(
-    id: string
-  ): Promise<_AsyncData<{ deleted: boolean } | null, FetchError<any> | null>> {
-    return await this._handleRequest<{ deleted: boolean }>(
-      this._endpoint + '/' + id,
-      'DELETE'
-    )
   }
 }
 
 export const productRepository = new ProductRepository()
+
