9f70af1503803643b9869fb6ea6125df46e6167f
Mazaev Andrew <mazaev2000@gmail.com>
2024-04-07 11:18:29 +0300

    feat: add all properties to BaseRepo

--- repository/baseRepository.ts
+++ repository/baseRepository.ts
@@ -3,19 +3,17 @@ import { FetchError } from 'ofetch'
 import httpService, { type IHttpService } from '~/services/httpService'
 import type { UseFetchOptions } from 'nuxt/app'
 
-interface IBaseRepository<T> {
-  // Сюда можно добавить другие методы для работы с API
-}
-
-class BaseRepository<T> implements IBaseRepository<T> {
+class BaseRepository<Res, Data> {
   constructor(protected readonly _endpoint: string) {}
 
   protected async _handleRequest<T, U = any>(
     url: string,
     method: 'GET' | 'POST' | 'PUT' | 'DELETE',
-    { body, ...options }: UseFetchOptions<T> & { body?: U } = {}
+    { body = undefined, ...options }: UseFetchOptions<T> = {}
   ): Promise<_AsyncData<T | null, FetchError<any> | null>> {
     try {
+      console.log({ body, options })
+
       const response: _AsyncData<T | null, FetchError<any> | null> = await (
         httpService as IHttpService
       )[method.toLowerCase()](url, body, options)
@@ -28,6 +26,59 @@ class BaseRepository<T> implements IBaseRepository<T> {
       }
       return error as _AsyncData<T | null, FetchError<any> | null>
     }
+  }
+
+  public async getAll<T = Res>(
+    options: UseFetchOptions<T> = {}
+  ): Promise<_AsyncData<T | null, FetchError<any> | null>> {
+    console.log(options)
+
+    return await this._handleRequest<T>(this._endpoint, 'GET', options)
+  }
+
+  public async getById<T = Res>(id: number, options: UseFetchOptions<T> = {}) {
+    return await this._handleRequest<T>(
+      this._endpoint + '/' + id,
+      'GET',
+      options
+    )
+  }
+
+  public async create<T = Res, U = Data>(
+    data: U extends globalThis.Ref<
+      Record<string, any> | BodyInit | null | undefined
+    >
+      ? U
+      : Record<string, any>,
+    options: UseFetchOptions<T> = {}
+  ) {
+    return await this._handleRequest<T, U>(this._endpoint, 'POST', {
+      body: data,
+      ...options,
+    })
+  }
+
+  public async update<T = Res, U = Data>(
+    id: number,
+    data: U extends globalThis.Ref<
+      Record<string, any> | BodyInit | null | undefined
+    >
+      ? U
+      : Record<string, any>,
+    options: UseFetchOptions<T> = {}
+  ) {
+    return await this._handleRequest<T, U>(this._endpoint + '/' + id, 'PUT', {
+      body: data,
+      ...options,
+    })
+  }
+
+  public async delete<T = Res>(id: number, options: UseFetchOptions<T> = {}) {
+    return await this._handleRequest<T>(
+      this._endpoint + '/' + id,
+      'DELETE',
+      options
+    )
   }
 }
 
