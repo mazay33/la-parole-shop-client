b9250f49f1aa19ddba107c78361e811ed480fb50
Gumrel <G0nte1@mail.ru>
2024-06-18 20:03:56 +0300

    dobavil infu v kartochki

--- src/components/home/main.vue
+++ src/components/home/main.vue
@@ -1,41 +1,46 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div class="max-w-[1180px] mx-auto">
-    <img
-      class="max-w-[780px] left-10 rotate-12 w-full absolute"
-      src="/public/flower.png"
-      alt=""
-    />
-    <div class="flex justify-between">
-      <div class="relative max-w-[610px] w-full">
-        <div class="z-10 text-[138px] cormorant-garamond-light font-300">
-          <p class="leading-28 tracking-normal">LA</p>
-          <p class="leading-28 tracking-normal ml-[72px] text-right">PAROLE</p>
-          <p class="leading-28 tracking-normal text-right">LINGIRIE</p>
-        </div>
-        <div class="text-right mt-10 leading-7 text-lg font-300 tracking-wider">
-          Нижнее белье ручной работы, <br />
-          раскрывающее твою <br />
-          чувственность
-        </div>
-      </div>
+	<div class="max-w-[1180px] mx-auto">
+		<img
+			class="max-w-[780px] left-10 rotate-12 w-full absolute"
+			src="/public/flower.png"
+			alt=""
+		/>
+		<div class="flex justify-between">
+			<div class="relative max-w-[610px] w-full">
+				<div class="z-10 text-[138px] cormorant-garamond-light font-300">
+					<p class="leading-28 tracking-normal">LA</p>
+					<p class="leading-28 tracking-normal ml-[72px] text-right">PAROLE</p>
+					<p class="leading-28 tracking-normal text-right">LINGIRIE</p>
+				</div>
+				<div class="text-right mt-10 leading-7 text-lg font-300 tracking-wider">
+					Нижнее белье ручной работы, <br />
+					раскрывающее твою <br />
+					чувственность
+				</div>
+			</div>
 
-      <div class="">
-        <img
-          class="max-w-[520px] w-full"
-          src="https://optim.tildacdn.com/tild6230-3562-4239-a662-653730636539/-/format/webp/IMG_7270_1.jpg"
-          alt=""
-        />
-      </div>
-    </div>
-  </div>
+			<div class="">
+				<img
+					class="max-w-[520px] w-full"
+					src="https://optim.tildacdn.com/tild6230-3562-4239-a662-653730636539/-/format/webp/IMG_7270_1.jpg"
+					alt=""
+				/>
+			</div>
+		</div>
+		<NuxtLink
+			to="product/components"
+			class="text-lg font-300"
+			>В каталог</NuxtLink
+		>
+	</div>
 </template>
 
 <style scoped>
-  .cormorant-garamond-light {
-    font-family: 'Cormorant Garamond', serif;
-    font-weight: 300;
-    font-style: normal;
-  }
+.cormorant-garamond-light {
+	font-family: 'Cormorant Garamond', serif;
+	font-weight: 300;
+	font-style: normal;
+}
 </style>
--- src/modules/product/pages/[id].vue
+++ src/modules/product/pages/[id].vue
@@ -8,6 +8,17 @@ const productId = useRoute().params.id as string;
 const { data: product } = await apiService.product.getProductById(productId, {
 	lazy: true,
 });
+
+const imgNum = ref(0);
+
+const cupSizes = product.value?.cup_sizes.map(size => size.size) || [];
+const selectedCupSize = ref();
+
+const underbust_sizes = product.value?.underbust_sizes.map(size => size.size) || [];
+const selectedUnderbust_sizes = ref();
+
+const clothing_sizes = product.value?.clothing_sizes.map(size => size.size) || [];
+const selectedClothing_sizes = ref();
 </script>
 
 <template>
@@ -18,30 +29,106 @@ const { data: product } = await apiService.product.getProductById(productId, {
 				v-if="product"
 			>
 				<NuxtImg
-					:src="`${config.public.api.replace('/api/', '')}/uploads/${product.img[0]?.url}`"
+					:src="`${config.public.api.replace('/api/', '')}/uploads/${product.img[imgNum]?.url}`"
 					alt=""
 					weight="560"
 					height="745"
+					class="transition-opacity duration-500 ease-in-out"
 				/>
 			</div>
 			<div class="flex flex-col justify-start mx-5">
-				<h2>{{ product?.name }}</h2>
+				<h4>{{ product?.name }}</h4>
+
 				<p>Артикул: {{ product?.sku }}</p>
 				<p>{{ product?.price.toLocaleString() }} ₽</p>
+
 				<div v-if="product?.variations">
 					<p>Комплектация</p>
 					<div class="flex gap-2">
-						<Button v-for="variation in product?.variations">
+						<Button
+							class="bg-white text-black border-black"
+							v-for="variation in product?.variations"
+						>
 							{{ variation.name }}
 						</Button>
 					</div>
 				</div>
-				<div class="mt-5">
-					<Button class="uppercase px-[30px] py-4 font-500">Добавить в корзину</Button>
+
+				<div
+					class="mt-8"
+					v-if="product?.cup_sizes"
+				>
+					<p>Чашка бюста</p>
+					<Dropdown
+						:options="cupSizes"
+						v-model="selectedCupSize"
+						class="mt--3"
+					></Dropdown>
 				</div>
+
+				<div
+					class="mt-3"
+					v-if="product?.underbust_sizes"
+				>
+					<p>Объем под грудью</p>
+					<Dropdown
+						:options="underbust_sizes"
+						v-model="selectedUnderbust_sizes"
+						class="mt--3"
+					></Dropdown>
+				</div>
+
+				<div
+					class="mt-3"
+					v-if="product?.clothing_sizes"
+				>
+					<p>Трусики</p>
+					<Dropdown
+						:options="clothing_sizes"
+						v-model="selectedClothing_sizes"
+						class="mt--3"
+					></Dropdown>
+				</div>
+
+				<div class="mt-10">
+					<Button
+						class="uppercase px-[30px] py-4 font-500 bg-black border-black hover:bg-red-300 hover:border-red-300 rounded-xl"
+						>Добавить в корзину</Button
+					>
+				</div>
+				<P class="mt-10"
+					>Закажите
+					<NuxtLink
+						to="/poshiv"
+						class="text-red-300"
+						>пошив по индивидуальным меркам</NuxtLink
+					>
+					данного комплекта</P
+				>
+			</div>
+			<div class="flex flex-wrap max-w-[560px] max-h-[140px] w-full h-full gap-1 mt--9">
+				<NuxtImg
+					v-for="(img, i) in product.img"
+					:key="i"
+					:src="`${config.public.api.replace('/api/', '')}/uploads/${product.img[i]?.url}`"
+					alt=""
+					:class="[
+						'w-16 h-20 transition-opacity duration-400 ease-in-out hover:opacity-70 cursor-pointer',
+						imgNum === i ? 'border-debug' : '',
+					]"
+					@click="imgNum = i"
+				/>
 			</div>
 		</div>
+		<pre mt-10>
+			{{ product }}
+		</pre
+		>
 	</div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.border-debug {
+	border: 2px solid rgba(0, 0, 0, 0.2);
+}
+</style>
