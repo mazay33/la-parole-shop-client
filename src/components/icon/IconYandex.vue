c1c816f1ed58c2d0b0722ee0a2bb16861f5e2cec
mazay33 <mazaev2000@gmail.com>
2024-07-17 12:59:10 +0300

    feat: authModal

---
+++ src/components/icon/IconYandex.vue
@@ -0,0 +1,22 @@
+<template>
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width="24"
+		height="24"
+		fill="none"
+		viewBox="0 0 44 44"
+	>
+		<path
+			d="M22 43a21 21 0 1 0 0-42 21 21 0 0 0 0 42Z"
+			fill="#F8604A"
+		/>
+		<path
+			d="M25.3 35.13h4.57V8.86h-6.66c-6.7 0-10.22 3.44-10.22 8.5 0 4.02 1.93 6.43 5.37 8.88l-5.99 8.88h4.97L24 25.18l-2.32-1.54c-2.8-1.9-4.17-3.36-4.17-6.54 0-2.79 1.97-4.68 5.72-4.68h2.05v22.7h.01Z"
+			fill="#fff"
+		/>
+	</svg>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
---
+++ src/modules/auth/components/AuthModal.vue
@@ -0,0 +1,192 @@
+<script setup lang="ts">
+definePageMeta({
+	layout: 'auth',
+});
+const config = useRuntimeConfig();
+
+const emit = defineEmits(['@close']);
+
+const authStore = useAuthStore();
+
+const email = ref<string>('user@example.com');
+const password = ref<string>('password123');
+const loading = ref<boolean>(false);
+
+const login = async () => {
+	loading.value = true;
+	await authStore.login(email.value, password.value);
+	if (authStore.isAuthenticated) {
+		emit('@close');
+	}
+	loading.value = false;
+};
+
+const loginWithYadnex = async () => {
+	const previousRoute = useRouter().currentRoute.value.fullPath;
+
+	localStorage.setItem('previousRoute', previousRoute);
+
+	navigateTo(config.public.api + 'auth/yandex', { external: true });
+};
+</script>
+<template>
+	<div class="w-full surface-card px-3 py-2">
+		<div class="flex flex-col gap-2">
+			<label for="email">Электронная почта</label>
+			<InputText
+				v-model="email"
+				id="email"
+				aria-describedby="email"
+				placeholder="user@example.com"
+			/>
+			<small id="email"></small>
+		</div>
+
+		<div class="flex flex-col gap-2 w-full">
+			<label for="password">Пароль</label>
+			<Password
+				v-model="password"
+				toggleMask
+				class="password-input"
+				aria-describedby="password"
+				:feedback="false"
+			/>
+			<small
+				id="password"
+				class="text-red"
+			></small>
+		</div>
+
+		<div class="flex align-items-center justify-content-between mb-6">
+			<a class="font-medium no-underline text-right cursor-pointer">Забыли пароль?</a>
+		</div>
+		<Button
+			@click="login()"
+			:loading="loading"
+			class="w-full"
+			icon="pi pi-user"
+			label="Войти"
+		/>
+
+		<div
+			class="p-divider p-component p-divider-horizontal p-divider-solid p-divider-center my-6"
+			style="justify-content: center"
+		>
+			<div
+				class="p-divider-content"
+				data-pc-section="content"
+			>
+				<span class="font-normal text-sm">ИЛИ</span>
+			</div>
+		</div>
+		<div class="flex gap-4">
+			<Button
+				@click="loginWithYadnex()"
+				class="p-button p-component w-full p-button-secondary"
+				type="button"
+			>
+				<IconYandex class="mr-2" />
+				<span class="p-button-label text-black">Войти с помощью Yandex</span>
+			</Button>
+			<Button
+				@click="loginWithYadnex()"
+				class="p-button p-component w-full p-button-secondary"
+				type="button"
+			>
+				<IconGoogle class="mr-2" />
+				<span class="p-button-label text-black">Войти с помощью Google</span>
+			</Button>
+		</div>
+
+		<div class="mt-6 text-center">
+			Нет аккаунта?
+			<a
+				tabindex="0"
+				class="font-medium"
+				>Зарегеистрируйтесь</a
+			>
+		</div>
+	</div>
+	<!-- <div class="w-full mx-auto p-6">
+		<div class="mb-5">
+			<div class="text-3xl font-medium mb-3">Welcome Back</div>
+			<span class="font-medium mr-2">Don't have an account?</span
+			><a class="font-medium no-underline text-indigo-500 cursor-pointer">Create today!</a>
+		</div>
+		<div>
+			<label
+				for="email1"
+				class="block font-medium mb-2"
+				>Email</label
+			>
+			<InputText
+				v-model="email"
+				id="email1"
+				type="text"
+				placeholder="Email address"
+				class="w-full mb-3"
+			/>
+
+			<label
+				for="password1"
+				class="block font-medium mb-2"
+				>Password</label
+			>
+			<InputText
+				v-model="password"
+				id="password1"
+				type="password"
+				placeholder="Password"
+				class="w-full mb-3"
+			/>
+			<div class="flex align-items-center justify-content-between mb-6">
+				<div class="flex align-items-center">
+					<Checkbox
+						id="rememberme1"
+						:binary="true"
+						class="mr-2"
+					></Checkbox>
+					<label for="rememberme1">Remember me</label>
+				</div>
+				<a class="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot password?</a>
+			</div>
+
+			<Button
+				@click="login()"
+				:loading="loading"
+				type="submit"
+				label="Sign In"
+				icon="pi pi-user"
+				class="w-full"
+			></Button>
+
+			<nuxt-link :to="config.public.api + 'auth/google'">
+				<Button
+					:loading="loading"
+					type="submit"
+					label="Sign In with Google"
+					icon="pi pi-user"
+					class="w-full mt-4"
+				></Button>
+			</nuxt-link>
+
+			<nuxt-link :to="config.public.api + 'auth/yandex'">
+				<Button
+					:loading="loading"
+					type="submit"
+					label="Sign In with Yandex"
+					icon="pi pi-user"
+					class="w-full mt-4"
+				></Button>
+			</nuxt-link>
+		</div>
+	</div> -->
+</template>
+
+<style>
+.password-input {
+	input {
+		@apply w-full;
+	}
+}
+</style>
